3125b6a9ae82469f9555059a78fb1d4f5e786415
rchall2 <rchall2@crimson.ua.edu>
2024-11-11 23:39:06 -0600

    Added Menu Choice html pages and css file

--- client/resources/scripts/referralStatistics.js
+++ client/resources/scripts/referralStatistics.js
@@ -1,7 +1,7 @@
 //W3Schools bar chart
-const xValues = ["Google", "Social Media", "Magazine", "Recommendation ", "Other"];
-const yValues = [10, 14, 5, 9, 7];
-const barColors = ["red", "green","blue","orange","brown"];
+const xValues = ["Google", "Social Media", "Magazine", "Recommendation ", "Other"]
+const yValues = [10, 14, 5, 9, 7]
+const barColors = ["red", "green","blue","orange","brown"]
 
 new Chart("myChart", {
   type: "bar",
@@ -13,4 +13,4 @@ new Chart("myChart", {
     }]
   },
   options: {}
-});
+})
