f669bbfe7dda686b37cca749b799d71b4cf00e1e
Bailey Grace Liddle <baileygraceliddle@Baileys-MacBook-Air-2.local>
2024-11-18 00:21:57 -0600

    m

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d3f2c17b51c9fc0898cc5616d86c25144428e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77eae58388b6875f2db864136c4b92dac4a7cae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
